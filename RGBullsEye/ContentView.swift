0798963c3e9288ea6cdcacad19229d875b92cae9
FrancaTM <francatm@outlook.com>
2019-06-11 16:40:18 -0300

    add @State variables

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -9,17 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+    
     var body: some View {
         VStack {
             HStack {
                 // target color block
                 VStack {
-                    Rectangle()
+                    Rectangle().foregroundColor(Color(red: rTarget, green: gTarget, blue: bTarget, opacity: 1.0))
                     Text("match this color")
                 }
                 // guess color block
                 VStack {
-                    Rectangle()
+                    Rectangle().foregroundColor(Color(red: rGuess, green: gGuess, blue: bGuess, opacity: 1.0))
                     HStack {
                         Text("r")
                         Text("g")
@@ -40,7 +47,7 @@ struct ContentView: View {
 #if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
     }
 }
 #endif
