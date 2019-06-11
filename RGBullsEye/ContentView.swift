7cf0ebd548273a3c9c43cfaadfa272cbcba5fcd1
FrancaTM <francatm@outlook.com>
2019-06-11 16:24:55 -0300

    Initial Commit

---
+++ RGBullsEye/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  RGBullsEye
+//
+//  Created by Tulio Marcos Franca on 11/06/19.
+//  Copyright © 2019 Tulio Marcos Franca. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
